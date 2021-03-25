c8b6ce0d2430c1e1316a220b1eb5e43ab2acdb1e
Davys Lima <davysjunior08@hotmail.com>
2021-03-24 23:47:36 -0300

    Feature: Login & Logout

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   env: {
     customKey:
       "sk_test_51IYU78EO66ZMERDoioktuW93cD4vVOy6TUvzoNdpi2cUXNRZBwEpxfsILea04M4EGTMTQikuz9KFnAqOdn01hTiv00aGEuxhDF",
+    GITHUB_ID: "d61f0ab6531cd2d2e683",
+    GITHUB_SECRET: "2409037c2686cfcff6181f4b7f2b008ccd4f26e4",
   },
 };
--- src/components/Header/SigninButton/index.tsx
+++ src/components/Header/SigninButton/index.tsx
@@ -1,17 +1,29 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signin, useSession, signOut } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const [session] = useSession();
+
+  console.log(session);
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Davys lima <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name} <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signin("github")}
+    >
       <FaGithub color="#eba417" /> Sign in with GitHub
     </button>
   );
