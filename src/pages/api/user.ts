45a0c5042315cc3c3d79bcd5b04b47df125df7d9
Davys Lima <davysjunior08@hotmail.com>
2021-03-24 22:39:52 -0300

    Feature: API Routes NEXT js

---
+++ src/pages/api/user.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: "Davys" },
+    { id: 2, name: "Cele" },
+    { id: 3, name: "Lene" },
+  ];
+
+  return response.json(users);
+};
