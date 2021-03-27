502f47c8463f224baa90d6c34e845bf8355febd1
Davys Lima <davysjunior08@hotmail.com>
2021-03-27 13:19:50 -0300

    Feature: Listening other events

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -4,7 +4,8 @@ import { stripe } from "../../../services/stripe";
 
 export async function saveSubscription(
   subscriptionId: string,
-  customerId: string
+  customerId: string,
+  createAction = false
 ) {
   try {
     const userRef = await fauna.query(
@@ -23,9 +24,21 @@ export async function saveSubscription(
       price_id: subscription.items.data[0].price.id,
     };
 
-    await fauna.query(
-      q.Create(q.Collection("subscriptions"), { data: subscriptionData })
-    );
+    if (createAction) {
+      await fauna.query(
+        q.Create(q.Collection("subscriptions"), { data: subscriptionData })
+      );
+    } else {
+      await fauna.query(
+        q.Replace(
+          q.Select(
+            "ref",
+            q.Get(q.Match(q.Index("subscription_by_id"), subscriptionId))
+          ),
+          { data: subscriptionData }
+        )
+      );
+    }
   } catch (error) {
     console.error(error);
   }
