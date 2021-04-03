56d3eaaeb1664042f81016d9769a2e65a108e05e
Davys Lima <davysjunior08@hotmail.com>
2021-04-03 20:17:45 -0300

    Feature: assign validation

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -42,7 +42,17 @@ export const getServerSideProps: GetServerSideProps = async ({
   params,
 }) => {
   const session = await getSession({ req });
+
   const { slug } = params;
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
   const prismic = getPrismicClient(req);
 
