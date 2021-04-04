b7b62d5edcbe7ecf7c7a3be60c265869611703fe
Davys Lima <davysjunior08@hotmail.com>
2021-04-03 22:30:30 -0300

    Feature: Post preview

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -45,7 +45,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   const { slug } = params;
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: "/",
