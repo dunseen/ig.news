36cd3450f82e02a359e1048b38fb156186921564
Davys Lima <davys.lima-extern@renault.com>
2022-01-16 22:27:44 -0300

    fix: change env variable name stripe key

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 import { version } from "../../package.json";
 
-export const stripe = new Stripe(process.env.customKey, {
+export const stripe = new Stripe(process.env.STRIPE_CUSTOMKEY, {
   apiVersion: "2020-08-27",
   appInfo: {
     name: "Ignews",
